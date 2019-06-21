ebeccaba8f5f5b2a6110cc06e44173a5f866d2f9
Dustin Wyatt <dustin.wyatt@gmail.com>
2019-06-21 15:22:19 -0500

    latest changes

---
+++ device_abnormal_state_monitor.py
@@ -0,0 +1,370 @@
+import abc
+import json
+import operator
+from operator import attrgetter
+from pathlib import Path
+from typing import Any, Callable, Optional, Sequence, Tuple, Union
+
+import appdaemon.plugins.hass.hassapi as hass
+import attr
+from boltons.typeutils import make_sentinel
+
+NOT_FOUND = make_sentinel("NOT_FOUND", "NOT_FOUND")
+DEFAULT = make_sentinel("DEFAULT", "DEFAULT")
+
+
+def get_nested_attr(obj, attribute: str, default=DEFAULT):
+    """
+    Get a named attribute from an object.
+
+    `get_nested_attr(x, 'a.b.c.d')` is equivalent to `x.a.b.c.d`.
+
+    When a default argument is given, it is returned when any attribute in the chain
+    doesn't exist; without it, an exception is raised when a missing attribute is
+    encountered.
+    """
+    getter = attrgetter(attribute)
+    try:
+        return getter(obj)
+    except AttributeError:
+        if default != DEFAULT:
+            return default
+        else:
+            raise
+
+
+StatesType = Sequence[str]
+
+
+def no_ok_states(
+    instance: "SimpleEntityState", attribute: str, value: StatesType
+) -> None:
+    if value and instance.ok_states:
+        raise ValueError("Cannot provide fault_states if also providing `ok_states`")
+
+
+def no_fault_states(
+    instance: "SimpleEntityState", attribute: str, value: StatesType
+) -> None:
+    if value and instance.fault_states:
+        raise ValueError("Cannot provide ok_states if also providing `fault_states`")
+
+
+def message_optional(
+    instance: "SimpleEntityState", attribute: str, value: StatesType
+) -> None:
+    if not instance.is_ok_when and value is None:
+        raise ValueError(f"Must provide message if not providing `is_ok_when`. ")
+
+
+CheckerReturnType = Tuple[bool, str]
+GetIsOkCallableType = Callable[["EntityState", Any, hass.Hass], CheckerReturnType]
+
+
+@attr.s(auto_attribs=True)
+class SimpleEntityState:
+    entity: str
+    entity_attr: str = "state"
+    #: If the value of this entity's state is in `fault_states`, we have a failure.
+    #: Note that you should only provide `fault_states` OR `ok_states`.
+    fault_states: StatesType = attr.ib(validator=no_ok_states, default=None)
+
+    #: If the value of this entity's state is *not* in `ok_states`, we have a
+    #: failure.  Note that you should only provide `fault_states` OR `ok_states`.
+    ok_states: StatesType = attr.ib(validator=no_fault_states, default=None)
+
+    ok_msg: Optional[str] = attr.ib(default=None)
+    fail_msg: Optional[str] = attr.ib(default=None)
+
+    @property
+    def entity_accessor(self):
+        return f"{self.entity}.{self.entity_attr}"
+
+
+@attr.s(auto_attribs=True)
+class AdvancedEntityState:
+    """
+    Describes how to check the state of an entity.
+    """
+
+    entity: str
+    entity_attr: str = "state"
+
+    #: If provided we use this callable to check the value.  The callable is called
+    #: with this EntityState, value we're checking, and the appdaemon api instance
+    is_ok_when: Optional[GetIsOkCallableType] = attr.ib(default=None)
+
+    @property
+    def entity_accessor(self):
+        return f"{self.entity}.{self.entity_attr}"
+
+
+class Checker(abc.ABC):
+    """
+    Checker instances are used to validate some attribute of the state of an entity.
+
+    Instances of this class are callables that take the arguments as shown on the
+    `__call__` method.
+
+    Implementing classes must override the `get_is_ok()` method.  This method is used
+    when you call an instance of this class.  As such you can count on having access
+    to the instance variables shown in the `__call__` method.  This should be :
+
+    self.es: the `AdvancedEntityState` which contains this checker
+    self.actual_val: the actual value in the state
+    self.expected_values: The values we're expecting.  of course, this is set in the
+        __init__ method, so if you override that, you may have something else.
+    self.ha: An instance of the appdaemon HA class.
+
+    Other useful methods that you may or may not want to override:
+
+    get_fail_msg() / get_ok_msg()
+    =============================
+    Gets the corresponding message to display/send/notify.
+
+    """
+
+    NOT_CALLED = make_sentinel("NOT_CALLED", "NOT_CALLED")
+
+    def __init__(
+        self, expected_values: Sequence[Any], fail_msg: str = None, ok_msg: str = None
+    ) -> None:
+        """
+        Set up checker with the values you're expecting to find.
+
+        This is just an example implementation, you can of course override init with
+        whatever logic and data you want.
+
+        When instantiating the class you can provide functions that
+
+        :param expected_values: A sequence of expected values.
+        :param fail_msg: Provide your own "static" failure message.  You can create
+            more dynamic messages by overriding `get_fail_msg()`.
+        :param ok_msg: Provide your own "static" ok message.  You can create
+            more dynamic messages by overriding `get_ok_msg()`.
+        """
+        self.expected_values = expected_values
+        self.static_fail_msg = fail_msg
+        self.static_ok_msg = ok_msg
+
+        self.es: Union[SimpleEntityState, Checker.NOT_CALLED] = Checker.NOT_CALLED
+        self.actual_val: Union[Any, Checker.NOT_CALLED] = Checker.NOT_CALLED
+        self.ha: Union[hass.Hass, Checker.NOT_CALLED] = Checker.NOT_CALLED
+
+    @abc.abstractmethod
+    def get_is_ok(self) -> bool:
+        """
+        Whether our data passed the check or not.
+        """
+        ...
+
+    def _validate_called(self) -> None:
+        """Helper method to make sure this instance has been called."""
+        assert all(
+            (
+                self.es != Checker.NOT_CALLED,
+                self.actual_val != Checker.NOT_CALLED,
+                self.ha != Checker.NOT_CALLED,
+            )
+        ), "Must call checker instance first."
+
+    def get_fail_msg(self):
+        self._validate_called()
+        return (
+            getattr(self, "static_fail_msg", None)
+            or f"{self.es.entity} failed check with a current value of `{self.actual_val}`."
+        )
+
+    def get_ok_msg(self):
+        self._validate_called()
+        return getattr(self, "static_ok_msg", None) or f"{self.es.entity} passed check."
+
+    def get_msg(self, is_ok: bool) -> str:
+        """
+        Get the failure or success message.
+
+        :param is_ok: Whether our data passed the check.
+        """
+        return self.get_ok_msg() if is_ok else self.get_fail_msg()
+
+    def __call__(self, es: "AdvancedEntityState", actual_val: str, ha: hass.Hass):
+        self.es = es
+        self.actual_val = actual_val
+        self.ha = ha
+
+        is_ok = self.get_is_ok()
+
+        return is_ok, self.get_msg(is_ok)
+
+
+class is_(Checker):
+    def __init__(
+        self, comparison: str, to: Any, converter: Callable[[Any], Any] = None
+    ) -> None:
+        """
+        Creates a callable that compares an expected value to an actual value.
+
+
+        :param to: The value we're expecting.
+        :param comparison: The comparison we're wanting to do.  Must be a string
+            representing the name of a function in the `operator` module.  For example,
+            `lt` for a less-than comparison
+        :param converter: An optional callable that takes the actual value and
+            converts it to another value before comparison to the expected value.
+        """
+        assert comparison in dir(
+            operator
+        ), f"`{comparison}` must be name of a function in the `operator` module."
+
+        if converter is None:
+            converter = lambda x: x
+
+        assert callable(
+            converter
+        ), "Converter must be a callable that takes a value and returns a value."
+
+        self.operation = getattr(operator, comparison)
+        self.converter = converter
+        self.expected_val = to
+
+    def get_is_ok(self) -> bool:
+        return self.operation(self.converter(self.actual_val), self.expected_val)
+
+
+class is_one_of(Checker):
+    def get_is_ok(self) -> bool:
+        return self.actual_val in self.expected_values
+
+
+class is_not_one_of(Checker):
+    def get_is_ok(self) -> bool:
+        return self.actual_val not in self.expected_values
+
+
+ENTITY_STATES = [
+    AdvancedEntityState(
+        entity="binary_sensor.front_door_camera_online", is_ok_when=is_one_of(["on"])
+    ),
+    AdvancedEntityState(
+        entity="binary_sensor.garage_camera_online", is_ok_when=is_one_of(["on"])
+    ),
+    AdvancedEntityState(
+        entity="sensor.xiaomi_flood_sensor_1_link_quality",
+        is_ok_when=is_not_one_of(["unknown"]),
+    ),
+    AdvancedEntityState(
+        entity="camera.front_door", is_ok_when=is_one_of(["recording"])
+    ),
+    AdvancedEntityState(
+        entity="light.morgans_bedside_lamp", is_ok_when=is_one_of(["off", "on"])
+    ),
+    AdvancedEntityState(
+        entity="switch.zooz_unknown_type2400_id2400_switch",
+        is_ok_when=is_one_of("off", "on"),
+    ),
+    AdvancedEntityState(
+        entity="water_heater.heat_pump_water_heater_gen_4",
+        is_ok_when=is_not_one_of(["unavailable"]),
+    ),
+    AdvancedEntityState(
+        entity="sensor.xiaomi_click_1_battery",
+        is_ok_when=is_("gt", 20, lambda x: int(float(x))),
+    ),
+    AdvancedEntityState(
+        entity="sensor.xiaomi_click_1_link_quality",
+        is_ok_when=is_("gt", 35, lambda x: int(float(x))),
+    ),
+    AdvancedEntityState(
+        entity="cover.garage_door_opener",
+        is_ok_when=is_one_of("open", "closed", "closing"),
+    ),
+    AdvancedEntityState(
+        entity="light.honeywell_hall", is_ok_when=is_one_of("on", "off")
+    ),
+    AdvancedEntityState(
+        entity="light.silver_lamp", is_ok_when=is_one_of(["on", "off"])
+    ),
+    AdvancedEntityState(
+        entity="light.silver_lamp_bulb_1", is_ok_when=is_one_of(["on", "off"])
+    ),
+    AdvancedEntityState(
+        entity="light.silver_lamp_bulb_2", is_ok_when=is_one_of(["on", "off"])
+    ),
+    AdvancedEntityState(
+        entity="lock.schlage_allegion_be469_touchscreen_deadbolt_locked",
+        is_ok_when=is_one_of(["locked", "unlocked"]),
+    ),
+]
+
+
+def get_es(entity_attr: str):
+    found = []
+    for es in ENTITY_STATES:
+        if entity_attr == es.entity:
+            # print(f"{entity_attr} is in {es.entity}")
+            found.append(es)
+
+    assert len(found), f"No matching EntityState for {entity_attr}"
+    return found
+
+
+class AbnormalStateMonitor(hass.Hass):
+    def initialize(self):
+        for es in ENTITY_STATES:
+            self.log(f"registering listener for {es.entity}")
+            self.listen_state(self.state_listener, es.entity, es=es)
+
+        es = get_es("binary_sensor.front_door_camera_online")[0]
+        self.log(self.is_ok(es))
+
+        p: Path = Path(__file__).resolve().parent / "state.json"
+        with p.open("w") as f:
+            json.dump(self.get_state(), f, indent=4, sort_keys=True)
+
+    def state_listener(self, entity, attribute, old, new, kwargs):
+        es = kwargs.get("es", None)
+        if not es:
+            self.log("State listener fired without an attached EntityState", "WARNING")
+        else:
+            ok, msg = self.is_ok(es)
+
+            if not ok:
+                self.notify(msg, name="main_html")
+                self.log(msg, "WARNING")
+
+    def is_ok(
+        self, es: Union[SimpleEntityState, AdvancedEntityState]
+    ) -> Tuple[bool, str]:
+        """
+        Checks an entity's state.
+
+        Returns a tuple.  First element is a bool where True means everything is
+        fine, and False means OH NOES.  Second element of the tuple is a message
+        describing the state.
+        """
+        value = get_nested_attr(self.entities, es.entity_accessor, default=NOT_FOUND)
+
+        if value == NOT_FOUND:
+            err = f"Cannot find `{es.entity_attr}`"
+            self.log(err, "ERROR")
+            return False, err
+
+        # bypass our later simplistic checks and use the checker callable
+        if hasattr(es, "is_ok_when") and callable(es.is_ok_when):
+            return es.is_ok_when(es, value, self)
+
+        def get_msg(passed: bool) -> str:
+            ok_msg = es.ok_msg or "passed check"
+            fail_msg = es.fail_msg or "failed check"
+            return (
+                f"{es.entity_accessor}: {ok_msg}"
+                if passed
+                else f"{es.entity_accessor}: {fail_msg}"
+            )
+
+        if es.fault_states:
+            passed = value not in es.fault_states
+        else:
+            passed = value in es.ok_states
+
+        return passed, get_msg(passed)
--- percent_scheduler.py
+++ percent_scheduler.py
@@ -1,17 +1,17 @@
 """
-Provides the `PercentSchedule` appdaemon app that will call `turn_on` and `turn_off` to keep a
-device on for X percent of the time.
+Provides the `PercentSchedule` appdaemon app that will call `turn_on` and `turn_off` to
+keep a device on for X percent of the time.
 
 The configuration includes:
 
     `device`: the hass entity_id of the device we want to toggle on and off
-    `percent: the percentage of time, expressed as a float from 0 to 1, that we want the device
-    to be on.
+    `percent: the percentage of time, expressed as a float from 0 to 1, that we want the
+        device to be on.
     `min_on_seconds`: the number of seconds the device will be on each time it comes on.
     `percent_state`: A hass state entity to get percent run time from.  If provided,
-    will override `percent`.
-    `min_on_seconds_state`: A hass state entity to get number of seconds the device will be on.
-    If provided will override `percent`.
+        will override `percent`.
+    `min_on_seconds_state`: A hass state entity to get number of seconds the device will
+        be on. If provided will override `percent`.
 
 For example, if we have a configuration like this:
 
@@ -21,8 +21,9 @@ percent: .25
 min_on_seconds: 100
 ```
 
-That configuration will cause `switch.basement_fan` to be turned on for 100 seconds, then it will
-shut off for 300 seconds, then it will turn back on for 100 seconds, and repeat for infinity.
+That configuration will cause `switch.basement_fan` to be turned on for 100 seconds,
+then it will shut off for 300 seconds, then it will turn back on for 100 seconds,
+and repeat for infinity.
 
 (100 seconds is 25% of 100 + 300 seconds)
 """
@@ -39,47 +40,55 @@ def get_seconds_off_per_on_second(percent: float) -> float:
     return off_seconds / on_seconds
 
 
-def get_on_off_time(percent: float,
-                    min_on_seconds: float = 60) -> Tuple[float, float]:
-    return (float(min_on_seconds),
-            get_seconds_off_per_on_second(percent) * min_on_seconds)
+def get_on_off_time(percent: float, min_on_seconds: float = 60) -> Tuple[float, float]:
+    return (
+        float(min_on_seconds),
+        get_seconds_off_per_on_second(percent) * min_on_seconds,
+    )
 
 
+# noinspection PyAttributeOutsideInit
 class PercentScheduler(hass.Hass):
-    PERCENT_STATE_KEY: ClassVar[str] = 'percent_state'
-    PERCENT_KEY: ClassVar[str] = 'percent'
-    MIN_ON_SECONDS_STATE_KEY: ClassVar[str] = 'min_on_seconds_state'
-    MIN_ON_SECONDS_KEY: ClassVar[str] = 'min_on_seconds'
+    PERCENT_STATE_KEY: ClassVar[str] = "percent_state"
+    PERCENT_KEY: ClassVar[str] = "percent"
+    MIN_ON_SECONDS_STATE_KEY: ClassVar[str] = "min_on_seconds_state"
+    MIN_ON_SECONDS_KEY: ClassVar[str] = "min_on_seconds"
 
     def initialize(self):
         self._timers = []
         self.on_then_off()
         turn_off_in, off_for = get_on_off_time(self.percent, self.min_on_seconds)
-        self.log(f'PercentScheduler running {self.device} for {self.percent * 100} '
-                 f'percent of the time.  It will be on for {turn_off_in} seconds and off '
-                 f'for {off_for} seconds.')
+        self.log(
+            f"PercentScheduler running {self.device} for {self.percent * 100} "
+            f"percent of the time.  It will be on for {turn_off_in} seconds and off "
+            f"for {off_for} seconds."
+        )
         percent_entity = self.args.get(self.PERCENT_STATE_KEY)
         if percent_entity:
-            self.log(f'Tracking percent from {percent_entity}')
+            self.log(f"Tracking percent from {percent_entity}")
             self.listen_state(self.track_state, entity=percent_entity)
         min_on_seconds_entity = self.args.get(self.MIN_ON_SECONDS_STATE_KEY)
         if min_on_seconds_entity:
-            self.log(f'Tracking min on seconds from {min_on_seconds_entity}')
+            self.log(f"Tracking min on seconds from {min_on_seconds_entity}")
             self.listen_state(self.track_state, entity=min_on_seconds_entity)
 
-    def track_state(self, entity: Optional[str],
-                    attribute: Optional[str],
-                    old: Any, new: Any, kwargs: Mapping[str, Any]):
+    def track_state(
+        self,
+        entity: Optional[str],
+        attribute: Optional[str],
+        old: Any,
+        new: Any,
+        kwargs: Mapping[str, Any],
+    ):
         percent_entity = self.args.get(self.PERCENT_STATE_KEY)
         min_on_seconds_entity = self.args.get(self.MIN_ON_SECONDS_STATE_KEY)
-        self.log(f'{entity} changed from {old} to {new}')
-        if (entity == percent_entity) \
-                or (entity == min_on_seconds_entity):
-            self.log('hass changed config')
+        self.log(f"{entity} changed from {old} to {new}")
+        if (entity == percent_entity) or (entity == min_on_seconds_entity):
+            self.log("hass changed config")
             for handle in self._timers:
-                self.log('Canceling existing timer.')
+                self.log("Canceling existing timer.")
                 self.cancel_timer(handle)
-            self.log('Rescheduling with new config.')
+            self.log("Rescheduling with new config.")
             self.on_then_off()
 
     @property
@@ -100,26 +109,26 @@ class PercentScheduler(hass.Hass):
 
     @property
     def device(self):
-        return self.args['device']
+        return self.args["device"]
 
     def get_on_off_time(self):
-        percent_entity = self.args.get(self.PERCENT_STATE_KEY)
         turn_off_in, off_for = get_on_off_time(self.percent, self.min_on_seconds)
         return turn_off_in, off_for
 
     def on_then_off(self, *args, **kwargs):
         turn_off_in, off_for = get_on_off_time(self.percent, self.min_on_seconds)
 
-        if self.get_state(self.device) != 'on':
-            self.log(f'Turning {self.device} on.', 'DEBUG')
+        if self.get_state(self.device) != "on":
+            self.log(f"Turning {self.device} on.", "DEBUG")
             self.turn_on(self.device)
 
-        self.log(f'Scheduling turn off of {self.device} '
-                 f'in {turn_off_in} seconds.')
+        self.log(f"Scheduling turn off of {self.device} " f"in {turn_off_in} seconds.")
         self._timers = [self.run_in(self.do_turn_off, turn_off_in)]
 
-        self.log(f'Scheduling turn on of {self.device} '
-                 f'in {turn_off_in + off_for} seconds.')
+        self.log(
+            f"Scheduling turn on of {self.device} "
+            f"in {turn_off_in + off_for} seconds."
+        )
         self._timers.append(self.run_in(self.on_then_off, turn_off_in + off_for))
 
     def do_turn_off(self, *args, **kwargs):
